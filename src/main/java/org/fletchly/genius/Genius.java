b6eb5c135f4e00014c85e0f9448ef2fe8bcc486a
Owen Mount <96270460+fletchly@users.noreply.github.com>
2025-04-21 10:12:55 -0700

    Paper plugin scaffolding

---
+++ src/main/java/org/fletchly/genius/Genius.java
@@ -0,0 +1,20 @@
+package org.fletchly.genius;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Genius extends JavaPlugin
+{
+
+    @Override
+    public void onEnable()
+    {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable()
+    {
+        // Plugin shutdown logic
+    }
+}
