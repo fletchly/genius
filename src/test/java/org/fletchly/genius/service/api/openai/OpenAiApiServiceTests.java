7e5db124c349ae92b691c0cc56339947dd40b299
Owen Mount <96270460+fletchly@users.noreply.github.com>
2025-04-22 17:54:44 -0700

    Refactor tests to use system environment variables

--- src/test/java/org/fletchly/genius/service/api/openai/OpenAiApiServiceTests.java
+++ src/test/java/org/fletchly/genius/service/api/openai/OpenAiApiServiceTests.java
@@ -1,6 +1,5 @@
 package org.fletchly.genius.service.api.openai;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -13,13 +12,11 @@ public class OpenAiApiServiceTests
     @BeforeEach
     void setUp()
     {
-        var dotenv = Dotenv.configure().filename("test.env").load();
-
-        String apiKey = dotenv.get("API_KEY");
-        String model = dotenv.get("MODEL");
-        String baseUrl = dotenv.get("BASE_URL");
-        String instructions = dotenv.get("SYSTEM_CONTEXT");
-        int maxTokens = Integer.parseInt(dotenv.get("MAX_TOKENS"));
+        String apiKey = System.getenv("API_KEY");
+        String model = System.getenv("MODEL");
+        String baseUrl = System.getenv("BASE_URL");
+        String instructions = System.getenv("SYSTEM_CONTEXT");
+        int maxTokens = Integer.parseInt(System.getenv("MAX_TOKENS"));
 
         api = new OpenAiApiService(apiKey, baseUrl, model, instructions, maxTokens);
     }
