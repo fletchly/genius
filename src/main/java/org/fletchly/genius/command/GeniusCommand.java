2c4bae5d4f0143c7da2f060f18ec240f98ae4da4
Owen Mount <96270460+fletchly@users.noreply.github.com>
2025-11-14 11:54:27 -0700

    Change command structure from method to property

--- src/main/java/org/fletchly/genius/command/GeniusCommand.java
+++ src/main/java/org/fletchly/genius/command/GeniusCommand.java
@@ -4,6 +4,7 @@ import com.mojang.brigadier.Command;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.tree.LiteralCommandNode;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
 import net.kyori.adventure.sound.Sound;
@@ -25,8 +26,12 @@ import static net.kyori.adventure.sound.Sound.Source.MASTER;
 import static net.kyori.adventure.text.Component.text;
 import static net.kyori.adventure.text.format.NamedTextColor.*;
 
+/**
+ * /genius command structure and logic
+ */
 public class GeniusCommand {
-    private static final ComponentBuilder<TextComponent, TextComponent.Builder> ChatName = text()
+    // Name to appear in chat when sending responses
+    private static final ComponentBuilder<TextComponent, TextComponent.Builder> chatName = text()
             .content("[")
             .append(text(Objects.requireNonNullElse(
                     Genius
@@ -37,25 +42,35 @@ public class GeniusCommand {
                     GREEN))
             .append(text("] "));
 
-    public static LiteralArgumentBuilder<CommandSourceStack> get() {
-        return Commands.literal("genius")
-                .requires(sender -> sender.getSender().hasPermission("genius"))
-                .then(Commands.argument("prompt", StringArgumentType.greedyString())
-                        .executes(GeniusCommand::execute));
-    }
+    // Command structure
+    private static final LiteralCommandNode<CommandSourceStack> command = Commands.literal("genius")
+            .requires(sender ->
+                    sender.getSender().hasPermission("genius"))
+            .then(Commands.argument("prompt", StringArgumentType.greedyString())
+                    .executes(GeniusCommand::execute)).build();
 
+    /**
+     * Execution logic for /genius command
+     * @param ctx Command context
+     * @return Commands.SINGLE_SUCCESS (1) always
+     */
     private static int execute(CommandContext<CommandSourceStack> ctx) {
+        // Get the current plugin instance, task scheduler, and command sender.
         final Genius instance = Genius.getInstance();
         final BukkitScheduler scheduler = instance.getServer().getScheduler();
         final CommandSender sender = ctx.getSource().getSender();
+
+        // Parse prompt
         String prompt = ctx.getArgument("prompt", String.class);
 
+        // Display prompt for sender
         final Component chatPrompt =
                 text(String.format("[%s] %s", sender.getName(), prompt), GRAY);
         sender.sendMessage(chatPrompt);
 
+        // Check that ollama service was initialized correctly
         if (instance.getOllamaService() == null) {
-            final Component chatResponse = ChatName
+            final Component chatResponse = chatName
                     .append(text("Genius is not set up correctly", RED))
                     .build();
             sender.sendMessage(chatResponse);
@@ -63,12 +78,15 @@ public class GeniusCommand {
             Genius.getInstance().getLogger().warning("Genius service is not initialized. Check your configuration");
         }
 
+        // Asynchronously make a request to Ollama.
         scheduler.runTaskAsynchronously(instance, () -> instance.getOllamaService().generateChat(prompt)
                 .exceptionally(ex -> {
+                    // Unwrap exception
                     Throwable cause = ex.getCause();
                     String userMessage;
                     Throwable exception;
 
+                    // Set qualified messages
                     switch (cause) {
                         case OllamaClient.OllamaHttpException httpEx -> {
                             userMessage = "Something went wrong when communicating with server. Please try again";
@@ -88,8 +106,9 @@ public class GeniusCommand {
                         }
                     }
 
+                    // Log error and inform user of failure
                     scheduler.runTask(instance, () -> {
-                        final Component chatResponse = ChatName
+                        final Component chatResponse = chatName
                                 .append(text(userMessage, RED))
                                 .build();
                         sender.sendMessage(chatResponse);
@@ -97,15 +116,17 @@ public class GeniusCommand {
                         Genius.getInstance().getLogger().warning("Genius ran into a problem: " + Arrays.toString(exception.getStackTrace()));
                     });
 
-                    return "Error";
+                    return "I'M DEAD X_x"; // never used
                 })
+                // Display response in chat
                 .thenAccept(response -> scheduler.runTask(instance, () -> {
-                    final Component chatResponse = ChatName
+                    final Component chatResponse = chatName
                             .append(text(response))
                             .build();
                     sender.sendMessage(chatResponse);
                     sender.playSound(Sound.sound(ENTITY_EXPERIENCE_ORB_PICKUP, MASTER, 1f, 1f), self());
-                })));
+                }))
+        );
         return Command.SINGLE_SUCCESS;
     }
 }
