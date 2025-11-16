fc37f8a82ba4d9568c77ad3b1f393578514c2e81
Owen Mount <96270460+fletchly@users.noreply.github.com>
2025-11-16 02:47:14 -0700

    Remove old data layer

--- src/main/java/org/fletchly/genius/data/dao/DaoException.java
+++
@@ -1,7 +0,0 @@
-package org.fletchly.genius.data.dao;
-
-public class DaoException extends RuntimeException {
-    public DaoException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
