5b5ab94aea887af215a3c1b26c343cbcaf2f2868
fletchly <owenfletchfletchly@gmail.com>
2025-12-31 13:27:29 -0500

    Update copyrights

--- src/main/kotlin/io/fletchly/genius/Genius.kt
+++ src/main/kotlin/io/fletchly/genius/Genius.kt
@@ -1,8 +1,7 @@
 /*
- * This file is part of Genius, licensed under the Apache License 2.0.
+ * This file is part of Genius, licensed under the Apache License 2.0
  *
  * Copyright (c) 2025 fletchly
- * Copyright (c) 2025 contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
